45fca0bf7ff1ad54799187eac4490d8dfe90cacd
Gav <gavin@parity.io>
2024-07-31 13:56:46 +0200

    Release script

--- release.sh
+++ release.sh
@@ -3,15 +3,29 @@
 [[ git diff --name-only | wc -l -gt 0 ]] && echo "There are uncommitted changes. Please commit or stash them before releasing." && exit 1
 
 VERSION=${cat VERSION}
-BRANCH=${git branch --show-current}
+OBRANCH=${git branch --show-current}
 RBRANCH="release-v$VERSION"
-echo "Releasing version $VERSION from branch $BRANCH into branch $RBRANCH..."
+echo "Releasing version $VERSION from branch $OBRANCH into branch $RBRANCH..."
 
 git checkout -b $RBRANCH
-touch VERSION_SUFFIX
+echo -n '' > VERSION_SUFFIX
 
+echo "Committing..."
 git commit -a -m "Release version $VERSION"
-git tag -a v$VERSION -m "Version $VERSION"
-git push --tags
 
-git checkout $BRANCH
+echo "Building PDF..."
+cp graypaper.tex graypaper-$VERSION.tex
+function cleanup() {
+    rm graypaper-$VERSION.aux graypaper-$VERSION.bbl graypaper-$VERSION.blg graypaper-$VERSION.log graypaper-$VERSION.out graypaper-$VERSION.tex* graypaper-$VERSION.run.xml graypaper-$VERSION.bcf
+    git checkout $OBRANCH
+}
+xelatex -halt-on-error graypaper-$VERSION > /dev/null || ( echo "Cannot build tex. Please fix before releasing." && cleanup && exit 1 )
+biber graypaper-$VERSION > /dev/null || ( echo "Cannot build bib. Please fix before releasing." && cleanup && exit 1 )
+xelatex -halt-on-error graypaper-$VERSION > /dev/null
+xelatex -halt-on-error graypaper-$VERSION > /dev/null
+
+echo "Tagging && pushing..."
+git tag -a v$VERSION -m "Version $VERSION"
+#git push --tags
+
+cleanup
